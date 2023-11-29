d0908ae925955b66c9972f22ee0598cc216b5bec
Orkun <orkun@email.com>
2023-11-29 17:35:06 +0300

    Update chatgpt-full-wider.user.js

--- chatgpt-full-wider.user.js
+++ chatgpt-full-wider.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ChatGPT Full Wider
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  increase chat gpt box width
 // @author       orknist
 // @match        https://chat.openai.com/*
@@ -15,8 +15,11 @@
     style.type = 'text/css';
     style.innerHTML = `
         .gizmo .gizmo\\:xl\\:max-w-\\[48rem\\] {
-            max-width: 148rem !important;
+            max-width: 480rem !important;
         }
+        .xl\\:max-w-\\[48rem\\] {
+        	max-width: 480rem;
+    	}
     `;
     document.head.appendChild(style);
 })();
